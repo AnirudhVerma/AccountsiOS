28c6291523c5b98963a673b47e2a673dbabce671
Anirudh Verma <anirudhv@integramicro.com>
2021-07-20 13:47:34 +0530

    Initial Commit

---
+++ Accounts/AccountsApp.swift
@@ -0,0 +1,17 @@
+//
+//  AccountsApp.swift
+//  Accounts
+//
+//  Created by Apple on 20/07/21.
+//
+
+import SwiftUI
+
+@main
+struct AccountsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
